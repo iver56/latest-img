ab760d36cf86ffc63a0faf9248dfdbfd81e11cbe
iver56 <iver56@hotmail.com>
2016-10-21 20:50:36 +0200

    Add image.php

---
+++ image.php
@@ -0,0 +1,31 @@
+<?php
+header("Content-type: image/jpeg");
+
+function getNewestFile($dir)
+{
+    $valid_file_types = array('jpg');
+
+    $newest_file = null;
+    $newest_file_time = -1;
+    foreach (scandir($dir) as $filename) {
+        $ext = strtolower(pathinfo($filename, PATHINFO_EXTENSION));
+        if (!in_array($ext, $valid_file_types)) {
+            continue;
+        }
+
+        $file_modification_time = filemtime($dir . '/' . $filename);
+        if (time() - $file_modification_time < 10) {
+            continue;
+        }
+        if ($file_modification_time > $newest_file_time) {
+            $newest_file_time = $file_modification_time;
+            $newest_file = $filename;
+        }
+    }
+
+    return $newest_file;
+}
+
+$newest_file = getNewestFile('.');
+
+readfile($newest_file);
